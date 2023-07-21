e5dfd1ef69606466bc2304201edd0106be6691a8
Hayama-r <82350403+Hayama-r@users.noreply.github.com>
2023-07-22 03:29:19 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,186 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo(".im1", {
+    x: -50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".im1",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+  
+gsap.fromTo(".tx1", {
+    x: 50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".tx1",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+
+
+  gsap.fromTo(".tx2", {
+    x: -50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".tx2",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+  
+gsap.fromTo(".im2", {
+    x: 50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".im2",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+
+
+
+  gsap.fromTo(".im3", {
+    x: -50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".im3",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+  
+gsap.fromTo(".tx3", {
+    x: 50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".tx3",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+
+
+
+  gsap.fromTo(".tx4", {
+    x: -50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".tx4",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+  
+gsap.fromTo(".im4", {
+    x: 50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".im4",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+
+  gsap.fromTo(".tx5", {
+    x: 50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".tx5",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+  
+gsap.fromTo(".im5", {
+    x: -50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".im5",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+
+  gsap.fromTo(".tx6", {
+    x: -50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".tx6",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
+  
+gsap.fromTo(".im6", {
+    x: 50,
+    opacity: 0,
+  }, {
+    scrollTrigger: {
+      trigger: ".im6",
+      start: "top center",
+      end:'bottom center',
+      //markers:true,
+      toggleActions:'play none none reverse',
+    },
+    x: 0,
+    opacity: 1,
+  });
